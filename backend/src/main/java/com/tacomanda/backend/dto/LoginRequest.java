2e83517b4298ef99ae7bea09698d2ff2b1a84b81
pablo1009010 <pablo1009010@gmail.com>
2026-08-03 20:33:41 -0600

    Update LoginRequest.java

--- backend/src/main/java/com/tacomanda/backend/dto/LoginRequest.java
+++ backend/src/main/java/com/tacomanda/backend/dto/LoginRequest.java
@@ -5,8 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 public class LoginRequest {
 
     @NotBlank(message = "El número de control es obligatorio")
-    private String noControl; // llega como texto; el controller lo valida/convierte
-
+    private String noControl;
     @NotBlank(message = "La contraseña es obligatoria")
     private String contrasena;
 
